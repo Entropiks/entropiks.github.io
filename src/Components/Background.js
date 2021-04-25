64e759714e9ad59d9dbbcbd91fabebbc7a7afd5f
Entropiks <mikecorseri@gmail.com>
2021-04-25 15:11:12 -0700

    coming soon

---
+++ src/Components/Background.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/Background.css'
+
+function Background() {
+  return (
+    <div className="comingsoon">
+      <h1>Coming<br/>Soon..</h1>
+    </div>
+  )
+}
+
+export default Background
